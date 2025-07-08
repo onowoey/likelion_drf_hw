95c0ff1365f77ca42430c42a05985f560d0de74f
onowoey <cg10046@naver.com>
2025-07-08 22:42:28 +0900

    url 수정

--- singer/urls.py
+++ singer/urls.py
@@ -7,5 +7,6 @@ app_name = "singer"
 urlpatterns = [
     path('', views.singer_list_create),
     path('<int:singer_id>', views.singer_detail_update_delete),
+    path('singer/<int:singer_id>', views.singer_detail_update_delete),
     path('<int:singer_id>/song', views.song_read_create),
 ]
